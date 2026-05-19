546e9d10e81f07619adf11ae0934c969fce55f7a
browman75 <browman75@gmail.com>
2026-05-19 14:51:12 +0800

    空

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // base: '/',
+    appType: 'spa',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
